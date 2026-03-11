1defb9120365987068534028a773661d82a9e417
bmcc81 <bmcc81@gmail.com>
2026-03-11 16:26:05 -0400

    Added auth /api/, fixed register.html, register.ts - validation, added global boostrap css

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+<<<<<<< Updated upstream
 import { RouterLink, RouterOutlet } from '@angular/router';
+=======
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './core/layout/navbar/navbar';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
